cd4c7de0bdd3442cd92db3d31738cce9611760fd
fares <fares@calms.eu>
2020-07-13 14:49:03 +0200

    Initial commit

---
+++ src/test/java/si/najemnina/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package si.najemnina.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
